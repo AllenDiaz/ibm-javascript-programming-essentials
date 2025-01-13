cf68bdcc24fd8ceabe9f51c32ee2c85321e926e6
allendiaz <allengdiaz2019@gmail.com>
2025-01-13 06:51:54 -0500

    add reset for exercise

--- followersCount/followers_count.js
+++ followersCount/followers_count.js
@@ -5,16 +5,23 @@ function increaseCount() {
     displayCount(); // Display the count
     checkCountValue(); // Check count value and display messages
   }
+function resetCount() {
+    count = 0;
+    displayCount()
+    resetAlert();
+}
 
 function displayCount() {
     document.getElementById('countDisplay').innerHTML=count; // Display the count in the HTML
 
     }
-
     function checkCountValue() {
         if (count === 10) {
           alert("Your Instagram post gained 10 followers! Congratulations!");
         } else if (count === 20) {
           alert("Your Instagram post gained 20 followers! Keep it up!");
         }
+      }
+      function resetAlert() {
+        alert("Followers has been reset!")
       }
